3bc7344222ff33cfc503dbd85becae6aba104af7
terrarome <tengku.alfarizi1@gmail.com>
2021-08-22 21:41:33 +0700

    Update Payment

--- bin/modules/v1/order/controller.js
+++ bin/modules/v1/order/controller.js
@@ -137,7 +137,8 @@ const create = async (payload) => {
       }
     }
 
-    let sub_total = payload.products[i].qty * currProduct.price;
+    let sub_total = payload.products[i].qty * payload.products[i].price;
+    console.log(sub_total)
     if(currProduct.disc > 0) {
     // if(currProduct.disc > 0 && new Date() < new Date(currProduct.exp_date)) {
       let subtract_price = currProduct.disc;
