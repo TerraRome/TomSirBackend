58f267f6b86fbd38da347d3a0c2af60d3d174f11
TerraRome <tengku.alfarizi1@gmail.com>
2022-12-08 22:24:41 +0700

    Update Base Url for Test

--- bin/configs/config.js
+++ bin/configs/config.js
@@ -6,7 +6,8 @@ const config = {
   port: 3000,
   secretKeyJwt: '588dc9c8-4c3c-4aa6-9fc8-e72f8cb1022c',
   secretKeyJwtRefresh: 'ca3cfaf9-c598-4ebb-a3de-9f97fbcef19b',
-  baseUrl: 'https://tomsir.fopini.id',
+  // baseUrl: 'https://tomsir.fopini.id',
+  baseUrl: 'https://7597-180-254-65-170.ngrok.io',
   mysql: {
     mysqlDb: 'db_tomsir',
     mysqlHost: 'localhost',
