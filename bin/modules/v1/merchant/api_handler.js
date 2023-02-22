77da10626bf8a34ee49a43decfa310c61d7d10d3
TerraRo <tengku.alfarizi1@gmail.com>
2023-02-22 17:08:19 +0700

    Add for Registration

--- bin/modules/v1/merchant/api_handler.js
+++ bin/modules/v1/merchant/api_handler.js
@@ -4,25 +4,25 @@ const reqModel = require('./request_model');
 const common = require('../../../helpers/common');
 const jwtAuth = require('../../../helpers/authentication');
 
-router.get('/', jwtAuth.verifyToken, jwtAuth.isSuperAdmin, async(req, res) => {
+router.get('/', jwtAuth.verifyToken, jwtAuth.isSuperAdmin, async (req, res) => {
   const payload = {
     ...req.query
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.getMerchants);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.getMerchants(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Get merchants fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -34,25 +34,25 @@ router.get('/', jwtAuth.verifyToken, jwtAuth.isSuperAdmin, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.get('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
+router.get('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async (req, res) => {
   const payload = {
     id: req.params.id,
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.getMerchant);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.getMerchant(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Get merchant fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -64,7 +64,7 @@ router.get('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.post('/', jwtAuth.verifyToken, jwtAuth.isSuperAdmin, async(req, res) => {
+router.post('/', jwtAuth.verifyToken, jwtAuth.isSuperAdmin, async (req, res) => {
   const payload = {
     ...req.body,
     image: req.files ? req.files.image : undefined,
@@ -73,19 +73,19 @@ router.post('/', jwtAuth.verifyToken, jwtAuth.isSuperAdmin, async(req, res) => {
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.create);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.create(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Create merchant fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -97,8 +97,38 @@ router.post('/', jwtAuth.verifyToken, jwtAuth.isSuperAdmin, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.put('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
-  if(req.decodedToken.role !== 'superadmin' && req.decodedToken.merchant.id !== req.params.id) {
+router.post('/regis', async (req, res) => {
+  const payload = {
+    ...req.body,
+  };
+  const validatePayload = await common.isValidPayload(payload, reqModel.create);
+  const postRequest = async (result) => {
+    if (result.err) {
+      return result;
+    }
+    return controller.create(result.data);
+  };
+  const sendResponse = async (result) => {
+    if (result.err) {
+      return res.status(result.err.code || 500).json({
+        success: false,
+        data: '',
+        message: result.err.message || 'Create merchant fail',
+        code: result.err.code || 500
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: result.data,
+      message: 'Regis merchant success',
+      code: 200
+    });
+  };
+  sendResponse(await postRequest(validatePayload));
+});
+
+router.put('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async (req, res) => {
+  if (req.decodedToken.role !== 'superadmin' && req.decodedToken.merchant.id !== req.params.id) {
     return res.status(403).json({
       success: false,
       data: '',
@@ -116,19 +146,19 @@ router.put('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.update);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.update(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Update merchant fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
--- bin/modules/v1/user/api_handler.js
+++ bin/modules/v1/user/api_handler.js
@@ -4,25 +4,25 @@ const reqModel = require('./request_model');
 const common = require('../../../helpers/common');
 const jwtAuth = require('../../../helpers/authentication');
 
-router.get('/', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
+router.get('/', jwtAuth.verifyToken, jwtAuth.isAdmin, async (req, res) => {
   const payload = {
     ...req.query
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.getUsers);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.getUsers(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Get users fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -34,25 +34,25 @@ router.get('/', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.get('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
+router.get('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async (req, res) => {
   const payload = {
     id: req.params.id,
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.getUser);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.getUser(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Get users fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -64,25 +64,25 @@ router.get('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.get('/profile', jwtAuth.verifyToken, async(req, res) => {
+router.get('/profile', jwtAuth.verifyToken, async (req, res) => {
   const payload = {
     email: req.decodedToken.email
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.getProfile);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.getProfile(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Get Profile fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -94,26 +94,26 @@ router.get('/profile', jwtAuth.verifyToken, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.put('/update-profile', jwtAuth.verifyToken, async(req, res) => {
+router.put('/update-profile', jwtAuth.verifyToken, async (req, res) => {
   const payload = {
     email: req.decodedToken.email,
     ...req.body
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.updateProfile);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.updateProfile(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Update profile fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -125,26 +125,26 @@ router.put('/update-profile', jwtAuth.verifyToken, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.put('/change-password', jwtAuth.verifyToken, async(req, res) => {
+router.put('/change-password', jwtAuth.verifyToken, async (req, res) => {
   const payload = {
     email: req.decodedToken.email,
     ...req.body
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.changePassword);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.changePassword(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Change password fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -156,25 +156,25 @@ router.put('/change-password', jwtAuth.verifyToken, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.post('/', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
+router.post('/', jwtAuth.verifyToken, jwtAuth.isAdmin, async (req, res) => {
   const payload = {
     ...req.body
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.create);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.create(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Create user fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -186,26 +186,56 @@ router.post('/', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.put('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
+router.post('/regis', async (req, res) => {
+  const payload = {
+    ...req.body
+  };
+  const validatePayload = await common.isValidPayload(payload, reqModel.create);
+  const postRequest = async (result) => {
+    if (result.err) {
+      return result;
+    }
+    return controller.create(result.data);
+  };
+  const sendResponse = async (result) => {
+    if (result.err) {
+      return res.status(result.err.code || 500).json({
+        success: false,
+        data: '',
+        message: result.err.message || 'Create user fail',
+        code: result.err.code || 500
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      data: result.data,
+      message: 'Create user success',
+      code: 200
+    });
+  };
+  sendResponse(await postRequest(validatePayload));
+});
+
+router.put('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async (req, res) => {
   const payload = {
     id: req.params.id,
     ...req.body
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.update);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.update(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Update user fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
@@ -217,25 +247,25 @@ router.put('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
   sendResponse(await postRequest(validatePayload));
 });
 
-router.delete('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async(req, res) => {
+router.delete('/:id', jwtAuth.verifyToken, jwtAuth.isAdmin, async (req, res) => {
   const payload = {
     id: req.params.id
   };
   const validatePayload = await common.isValidPayload(payload, reqModel.getUser);
   const postRequest = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return result;
     }
     return controller.deleteOne(result.data);
   };
   const sendResponse = async (result) => {
-    if(result.err) {
+    if (result.err) {
       return res.status(result.err.code || 500).json({
         success: false,
         data: '',
         message: result.err.message || 'Delete user fail',
         code: result.err.code || 500
-      }); 
+      });
     }
     return res.status(200).json({
       success: true,
